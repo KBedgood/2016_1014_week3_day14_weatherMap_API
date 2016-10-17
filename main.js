4cce3a3b8f8c80e507b56d6c6ec7adcd885702a1
Kristie A Bedgood <kristieabedgood@Kristies-MacBook-Pro.local>
2016-10-16 20:59:13 -0400

    no dice

--- main.js
+++ main.js
@@ -5,26 +5,26 @@ jQuery(function(){
   function getMyweather(query)
 
   $ajax({
-url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=${"text"}&units=imperial&cnt=7&APPID=a66f3e4a44d08030372b2690bc228aab',
-success: function successHandler (weather) {
-// console.log(weather);
+url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=${"text"}&units=imperial&cnt=7&APPID=a66f3e4a44d08030372b2690bc228aab' ,
+success: function successHandler(weather) {
+console.log(weather);
 
-       $("#placeholder").html("<div class='row'>");
+//        $("#placeholder").html("<div class='row'>");
 
-      // Loop through our array of gifs and put them on the site
-      weather.data.forEach(function(day){
-        $("#placeholder .row").append(`<div class="gif col-md-4"><img src="${PLACE FROM CONSOLE DATAgif.images.fixed_height.url}"></div>`);
-      })
+//       // Loop through our array of gifs and put them on the site
+//       weather.data.forEach(function(day){
+//         $("#placeholder .row").append(`<div class="gif col-md-4"><img src="${PLACE FROM CONSOLE DATAgif.images.fixed_height.url}"></div>`);
+//       })
 
       
-  }});
-}
+//   }});
+// }
 
 
-$("#button").on("click",function(){
-  var text =$("#search").val();
-  getMyWeather(text);
-};
+// $("#button").on("click",function(){
+//   var text =$("#search").val();
+//   getMyWeather(text);
+// };
 
 
 
